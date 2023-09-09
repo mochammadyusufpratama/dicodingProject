ecda7646255256e242383793a19c343dc52bbb38
MochammadYusuf <mochammadyusufpratama6@gmail.com>
2023-09-09 07:52:08 +0700

    first commit

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const ham = document.querySelector('#hamburger')
+const hamMenu = document.querySelector('#hamburger-menu')
+
+ham.addEventListener('click', function () {
+    ham.classList.toggle('hamburger-active')
+    hamMenu.classList.toggle('hidden');
+});
+
+window.addEventListener('click', function (e) {
+    if (e.target != ham && e.target != hamMenu) {
+        ham.classList.remove('hamburger-active')
+        hamMenu.classList.add('hidden');
+    }
+});
